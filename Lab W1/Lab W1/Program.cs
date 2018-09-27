94ebc6e66b3835b13ff68033a28688cd43c4e2d5
Eleasar <S01209223@acad.tri-c.edu>
2018-09-27 00:32:43 -0400

    create Lab W1

---
+++ Lab W1/Lab W1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_W1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Eleasar");
+        }
+    }
+}
